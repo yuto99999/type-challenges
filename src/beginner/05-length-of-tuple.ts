1f4a1c3f7fc7efed8a2836740dc94b436108a070
yuto99999 <myuto2003@gmail.com>
2024-11-09 19:13:47 +0900

    Edit Length<T>

--- src/beginner/05-length-of-tuple.ts
+++ src/beginner/05-length-of-tuple.ts
@@ -13,14 +13,18 @@ type spaceX = [
 type teslaLength = Length<typeof tesla>; // expected 4
 type spaceXLength = Length<spaceX>; // expected 5
 
+
+
 // 発想
 // 1. とりあえず配列に制限したいので、extends any[]を記述
 // 2. teslaの配列がconstアサーションにより読み取り専用の配列になっているので、制約の中にreadonlyを追加
 // 3. あとは要素の数を取得するだけ
 
-// 学び -> https://mosya.dev/blog/tc-tuple-length
 
-// T["length"] -> 配列の要素を取得する
+// 学び
+// T["length"] -> 配列の要素を取得する -> https://mosya.dev/blog/tc-tuple-length
+tesla["length"]; // expected 4
+
 
 // 復習
 // typeof演算子とは
@@ -30,3 +34,18 @@ const Tokyo = {
   population: 1000,
 };
 type CheckTokyo = typeof Tokyo; // <- {city: string; population: number;}
+
+// as constについて
+// as constの有無による違いは何か
+// ↓
+// ないver
+const NormalArr = ["Tokyo", "Osaka", "Nagoya"];
+NormalArr; // NormalArr: string[] (<- string型の配列としか認識されていない)
+
+// あるver
+const SpecialArr = ["Tokyo", "Osaka", "Nagoya"] as const;
+SpecialArr; // SpecialArr: readonly ["Tokyo", "Osaka", "Nagoya"]
+
+// おまけ(T[number])
+type V = (typeof SpecialArr)[number]; // type V = "Tokyo" | "Osaka" | "Nagoya"
+// 全てのインデックスの要素の型を、ユニオン型で返してくれる
