1b484b0ef9db8133fbdb33261946d83522146861
yuto99999 <myuto2003@gmail.com>
2024-11-13 23:02:53 +0900

    Edit Includes<T, U>

--- src/beginner/10-includes.ts
+++ src/beginner/10-includes.ts
@@ -18,10 +18,18 @@ type IncludesTest9 = Includes<[1], 1 | 2>; // false
 type IncludesTest10 = Includes<[1 | 2], 1>; // true
 type IncludesTest11 = Includes<[null], undefined>; //false
 
-// 処理に流れ
+
+
+// 処理の流れ
+// 1.T[number]で配列の要素をUnion型に変換
+// 2.マッピング型を作成(T = [1, 2, 3]の場合)
+// {1:true, 2:true, 3:true}
+// 3.Uでインデックスアクセス
+// 4.条件分岐で判定
+// trueではない場合にfalseを指定しないと、undefinedになってしまう。
+
 
 // 学び
-
 // インデックスアクセス型
 // 2つの表記法がある
 // 1.オブジェクト型["プロパティ名"]
@@ -29,9 +37,14 @@ type IPerson = { name: string; age: number };
 type IP = IPerson["name" | "age"]; // type IP = string | number
 
 // 2.配列型[number]
+const IPerson2 = ["Tom", "Judy", "Anna"] as const;
+type IP2 = (typeof IPerson2)[number]; //"Tom" | "Judy" | "Anna"
 
 
-//復習
+// 復習
+// T[number]
+// 配列やタプルの要素をユニオン型に変換する
+
 // keyof演算子
 // -> オブジェクトの全てのプロパティをユニオン型で返す
 type IFoo = { a: number; b: string; c: boolean };
