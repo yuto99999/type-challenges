0a56109f890c56259ed326f872020105e44194e7
yuto99999 <myuto2003@gmail.com>
2024-12-26 17:51:46 +0900

    Add OmitByType<T, U>

---
+++ src/medium/52-omit-by-type.ts
@@ -0,0 +1,18 @@
+// Tから型がUに適合しないプロパティだけを選び出す
+
+type OmitByType<T, U> = { [P in keyof T as T[P] extends U ? never : P]: T[P] };
+
+interface Model {
+  name: string;
+  count: number;
+  isReadonly: boolean;
+  isEnable: boolean;
+}
+
+type Test52_1 = OmitByType<Model, boolean>; // { name: string, count: number }
+type Test52_2 = OmitByType<Model, string>; // { count: number, isReadonly: boolean, isEnable: boolean }
+type Test52_3 = OmitByType<Model, number>; // { name: string, isReadonly: boolean, isEnable: boolean }
+
+
+// 復習
+// PickByTypeと同じ
